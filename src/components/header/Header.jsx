1885fc5914899a375040859f0b12ca14aed7bf2f
Jean Celin <jean_estudos@hotmail.com>
2023-10-06 10:57:59 -0300

    Criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,16 @@ import style from './Header.module.css'
 
 import Tittle from '../title/Title'
 import Subtitle from '../subTitle/Subtitle'
+import Icon from '../icon/Icon'
 
 function Header () {
   return (
     <div className={style.header}>
       <Tittle>Jogo da Velha</Tittle>
       <Subtitle>Criador por Jean Celin</Subtitle>
+      <div className={style.iconContent}>
+        <Icon iconName="github" link="https://github.com/JeanCelin" />
+      </div>
     </div>
     
   )
